934c34f221bb1a4e56c76099564bed16524b05c9
Daiyaz <uchihachikk3@gmail.com>
2025-01-31 16:46:30 +0700

    Add functions for random data generation and data display in lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -19,6 +19,7 @@ int main(){
 	double sum1[N] = {};
 	double sum2[M] = {};
 	double *dPtr = data[0];
+	
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
 	
@@ -32,3 +33,44 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void randData(double *arr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(arr+i) = rand()%101/100.0;
+    }
+}
+
+
+//เติมส่วนที่หายไปใน Function showData() โดยห้ามแก้ไขโครงสร้าง
+
+//เติมส่วนที่หายไปใน Function showData() โดยห้ามแก้ไขโครงสร้าง
+
+void showData(double *arr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(arr+i);
+        if(i%M==M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+
+void findRowSum(const double *arr,double *arr2,int N,int M){
+	for(int i =0;i<N;i++){
+		*(arr2+i) =0;
+		for(int j =0;j<M;j++){
+			*(arr2+i) += *(arr+i*M+j);
+		}
+	}
+} 
+void findColSum(const double *arr,double *arr2,int N,int M){
+	for(int i =0;i<M;i++){
+		*(arr2+i) =0;
+		for(int j =0;j<N;j++){
+			*(arr2+i) += *(arr+j*M+i);
+		}
+	}
+} 
+
+
+
